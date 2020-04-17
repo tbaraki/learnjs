2571ba253811c7cb0500a010405e51f407def856
Travis Baraki <travis.baraki@gmail.com>
2020-04-17 12:28:50 -0700

    easyhttp library complete

--- Sec7 AsyncAjaxFetchAPI/Custon HTTP Library/app.js
+++ Sec7 AsyncAjaxFetchAPI/Custon HTTP Library/app.js
@@ -1,13 +1,13 @@
 const http = new easyHTTP();
 
 // Get Posts
-http.get('https://jsonplaceholder.typicode.com/posts', function (err, posts) {
-	if (err) {
-		console.log(err);
-	} else {
-		console.log(posts);
-	}
-});
+// http.get('https://jsonplaceholder.typicode.com/posts', function (err, posts) {
+// 	if (err) {
+// 		console.log(err);
+// 	} else {
+// 		console.log(posts);
+// 	}
+// });
 
 // // Get single Post
 // http.get('https://jsonplaceholder.typicode.com/posts/1', function (err, post) {
@@ -18,4 +18,44 @@ http.get('https://jsonplaceholder.typicode.com/posts', function (err, posts) {
 // 	}
 // });
 
-// POST posts
+// create data
+const data = {
+	title: 'Custom Post',
+	body: 'This is a custom post.',
+};
+
+// create post
+// http.post('https://jsonplaceholder.typicode.com/posts', data, function (
+// 	err,
+// 	post
+// ) {
+// 	if (err) {
+// 		console.log(err);
+// 	} else {
+// 		console.log(post);
+// 	}
+// });
+
+// // create PUT - update post
+// http.put('https://jsonplaceholder.typicode.com/posts/1', data, function (
+// 	err,
+// 	post
+// ) {
+// 	if (err) {
+// 		console.log(err);
+// 	} else {
+// 		console.log(post);
+// 	}
+// });
+
+// delete post
+http.delete('https://jsonplaceholder.typicode.com/posts/1', function (
+	err,
+	response
+) {
+	if (err) {
+		console.log(err);
+	} else {
+		console.log(response);
+	}
+});
--- Sec7 AsyncAjaxFetchAPI/Custon HTTP Library/easyhttp.js
+++ Sec7 AsyncAjaxFetchAPI/Custon HTTP Library/easyhttp.js
@@ -28,9 +28,34 @@ easyHTTP.prototype.post = function (url, data, callback) {
 		callback(null, self.http.responseText);
 	};
 
-	this.send(JSON.stringify(data));
+	this.http.send(JSON.stringify(data));
 };
 
 // make PUT request
+easyHTTP.prototype.put = function (url, data, callback) {
+	this.http.open('PUT', url, true);
+	this.http.setRequestHeader('Content-type', 'application/json');
+
+	let self = this;
+	this.http.onload = function () {
+		callback(null, self.http.responseText);
+	};
+
+	this.http.send(JSON.stringify(data));
+};
 
 // make DELETE request
+easyHTTP.prototype.delete = function (url, callback) {
+	this.http.open('DELETE', url, true);
+
+	let self = this;
+	this.http.onload = function () {
+		if (self.http.status === 200) {
+			callback(null, 'Post deleted');
+		} else {
+			callback('Error: ' + self.http.status);
+		}
+	};
+
+	this.http.send();
+};
