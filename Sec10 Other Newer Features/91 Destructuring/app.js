1f673bf339be4ef5c9f6c859eec69a4c276ef9ef
Travis Baraki <travis.baraki@gmail.com>
2021-06-26 09:41:04 -0700

    clean up

--- Sec10 Other Newer Features/91 Destructuring/app.js
+++ Sec10 Other Newer Features/91 Destructuring/app.js
@@ -1,35 +1,57 @@
-// destructuring assignment
+// // destructuring assignment
 
-let a, b;
-[a, b] = [100, 200];
+// let a, b;
+// [a, b] = [100, 200];
 
-// rest pattern
-[a, b, ...rest] = [100, 200, 300, 400, 500];
+// // rest pattern
+// [a, b, ...rest] = [100, 200, 300, 400, 500];
 
-console.log(a);
-console.log(b);
-console.log(rest);
+// console.log(a);
+// console.log(b);
+// console.log(rest);
 
-({ a, b } = { a: 100, b: 200, c: 300, d: 400, e: 500})
+// ({ a, b } = { a: 100, b: 200, c: 300, d: 400, e: 500})
 
-console.log(a,b);
+// console.log(a,b);
 
-// array destructuring
-const people = ['John', 'Beth', 'Mike']
+// // array destructuring
+// const people = ['John', 'Beth', 'Mike']
 
-const [person1, person2, person3] = people;
+// const [person1, person2, person3] = people;
 
-console.log(person1,person2,person3);
+// console.log(person1,person2,person3);
 
 // parse array returned from function
-function getPeople () {
-    return ['John', 'Beth', 'Mike']
-}
+// function getPeople () {
+//     return ['John', 'Beth', 'Mike']
+// }
 
-let person1, person2, person3;
+// let person1, person2, person3;
 
-[person1, person2, person3] = getPeople();
+// [person1, person2, person3] = getPeople();
 
-console.log(person1,person2,person3);
+// console.log(person1,person2,person3);
 
 // object destructuring
+
+const person = {
+    name: 'John Doe',
+    age: 32,
+    city: 'Miami',
+    gender: 'Male',
+    sayHello: function(){
+        console.log('Hello');
+    }
+}
+
+//old es5
+// const name = person.name,
+//     age = person.age,
+//     city = person.city,
+
+// new es6 destructuring
+const { name, age, city, sayHello } = person
+
+console.log(name, age, city);
+sayHello();
+
